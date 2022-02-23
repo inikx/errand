e5a0c1093644f5f3626627f01c96de72f75fe88b
sahinurn <bishyasu@gmail.com>
2022-02-23 16:08:32 +0300

    add routes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const compression = require("compression");
 const userRoutes = require("./routes/userRoutes");
-
+const taskRoutes = require("./routes/taskRoutes");
+const groupRoutes = require("./routes/groupRoutes");
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(cors({ credentials: true, origin: true }));
 
 // Routes
 app.use("/api", userRoutes);
+app.use("/api/task", taskRoutes);
+app.use("/api/group", groupRoutes);
 
 // Handle undefined Routes
 app.use("*", (req, res, next) => {
