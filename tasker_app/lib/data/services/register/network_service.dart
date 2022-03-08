f185eb1497c57dab8448e6ca479b1c5f612777df
sahinurn <bishyasu@gmail.com>
2022-03-08 18:49:45 +0300

    add registration BLOC

---
+++ tasker_app/lib/data/services/register/network_service.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:tasker_app/constants/storage.dart';
+import 'package:tasker_app/constants/strings.dart';
+
+class RegisterNetworkService {
+  register(String username, String email, String password) async {
+    final response = await http.post(Uri.parse('$BASE_URL/api/login'),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(
+            {"username": username, "email": email, "password": password}));
+
+    return response;
+  }
+}
---
+++ tasker_app/lib/data/services/register/repository.dart
@@ -0,0 +1,12 @@
+import 'package:http/http.dart';
+import 'package:tasker_app/data/services/register/network_service.dart';
+
+class RegisterRepository {
+  final RegisterNetworkService networkService;
+  RegisterRepository({required this.networkService});
+
+  Future<Response> registerUser(
+      String username, String email, String password) async {
+    return await networkService.register(username, email, password);
+  }
+}
