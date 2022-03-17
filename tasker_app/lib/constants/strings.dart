49f17a7575f50c0ae5a7750142019f785179ded6
sahinurn <sahinurn@github.com>
2022-03-17 20:13:52 +0300

    add server ip

--- tasker_app/lib/constants/strings.dart
+++ tasker_app/lib/constants/strings.dart
@@ -2,4 +2,4 @@ const LOGIN = "/";
 const HOME = "/home";
 const REGISTER = "/register";
 const REGISTER_2ND = "/register_2nd";
-const BASE_URL = "http://10.0.2.2:3000";
+const BASE_URL = "http://194.67.119.179:3000";
