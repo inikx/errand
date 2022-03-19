ebeec2673585e7d1a416d7343221a7ce2b31fbdb
sahinurn <bishyasu@gmail.com>
2022-03-19 22:03:25 +0300

    fix login bloc

--- tasker_app/lib/bloc/login/login_state.dart
+++ tasker_app/lib/bloc/login/login_state.dart
@@ -9,22 +9,16 @@ class LoggingIn extends LogInState {}
 class LoggedIn extends LogInState {}
 
 class LogInInitial extends LogInState {
-  @override
-  // ignore: overridden_fields
   final LogInData data;
   LogInInitial({required this.data});
 }
 
 class LogInChanged extends LogInState {
-  @override
-  // ignore: overridden_fields
   final LogInData data;
   LogInChanged({required this.data});
 }
 
 class LogInError extends LogInState {
-  @override
-  // ignore: overridden_fields
   final LogInData data;
   LogInError({required this.data});
 }
