c9f26fbba80d25bd923022e15d1186a35885b60c
sahinurn <sahinurn@github.com>
2022-03-25 19:36:08 +0300

    add token push

--- tasker_app/lib/data/services/authentication/network_service.dart
+++ tasker_app/lib/data/services/authentication/network_service.dart
@@ -7,8 +7,10 @@ class AuthenticationNetworkService {
   auth() async {
     String? token = await storage.read(key: 'token');
     final response = await http.get(Uri.parse('$BASE_URL/api/authentication'),
-        headers: {"Content-Type": "application/json"});
-    var jsonResponse = jsonDecode(response.body);
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": token.toString()
+        },);
     if (response.statusCode == 200) {
       return true;
     } else {
