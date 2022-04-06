8d05446188872c5f859462ad42ba11519f70f683
sahinurn <bishyasu@gmail.com>
2022-04-07 01:25:11 +0300

    add project invites controlling

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -7,7 +7,9 @@ router.get("", auth, projectController.getProjects);
 router.post("/create", auth, projectController.createProject);
 router.put("/update", auth, projectController.updateProject);
 router.get("/users/:id", auth, projectController.getAllProjectUsers);
+router.get("/invites/all", auth, projectController.getInvitedProjects);
 router.post("/add/user", auth, projectController.addUserToProject);
+router.post("/update/invite", auth, projectController.updateProjectInvite);
 router.delete("/remove", auth, projectController.removeProject);
 
 module.exports = router;
